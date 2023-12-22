2a1cdd206913281cc9f05f5514880e2500212252
cwtpldev03 <cwtpldev03@codenxt.com>
2023-12-21 20:20:12 -0500

    Guessting game part 1

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main(){
+    println!("Guess the number !");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The Secret number is : {secret_number}");
+
+    loop {
+        println!("Please input your guess ");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Faild to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+
+        println!("Guessed number : {guess}");
+
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Equal => {
+                println!("You won");
+                break;
+            }
+        }
+    }
+}
